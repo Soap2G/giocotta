e8ef6d15054db0b13b05fbd4b7e0f8e723957c3d
Soap2G <49747129+Soap2G@users.noreply.github.com>
2023-12-11 17:50:19 +0100

    fix

--- generate-posts.js
+++ generate-posts.js
@@ -2,19 +2,30 @@ const fs = require('fs');
 const path = require('path');
 
 const postsDirectory = path.join(__dirname, 'public/posts');
+
+// Check if the directory exists
+if (!fs.existsSync(postsDirectory)) {
+  console.log('Posts directory not found. Creating it...');
+  fs.mkdirSync(postsDirectory);
+}
+
 const filenames = fs.readdirSync(postsDirectory);
 
-const posts = filenames.filter(file => file.endsWith('.md')).map(file => {
-  const slug = file.replace(/\.md$/, '');
-  return {
-    slug: slug,
-    title: slug.replace(/-/g, ' '), // Replace dashes with spaces, capitalize, etc.
-    path: `/posts/${file}`
-  };
-});
+let posts = [];
 
-// Sort posts by number :D
-posts.sort((a, b) => parseInt(a.slug, 10) - parseInt(b.slug, 10));
+if (filenames.length > 0) {
+  posts = filenames.filter(file => file.endsWith('.md')).map(file => {
+    const slug = file.replace(/\.md$/, '');
+    return {
+      slug: slug,
+      title: slug.replace(/-/g, ' '), // Replace dashes with spaces, capitalize, etc.
+      path: `/posts/${file}`
+    };
+  });
+
+  // Sort posts by number
+  posts.sort((a, b) => parseInt(a.slug, 10) - parseInt(b.slug, 10));
+}
 
 const content = `const posts = ${JSON.stringify(posts, null, 2)};\n\nexport default posts;`;
 fs.writeFileSync(path.join(__dirname, 'src/posts/posts.jsx'), content);
