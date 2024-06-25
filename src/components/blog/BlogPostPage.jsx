25175530869a6ea1c079ad7c92687aa757f8df9c
Giovanni Guerrieri <{ID}+{username}@users.noreply.github.com>
2024-06-26 01:40:35 +0200

    adding 1

--- src/components/blog/BlogPostPage.jsx
+++ src/components/blog/BlogPostPage.jsx
@@ -6,6 +6,68 @@ import posts from '../../posts/posts';
 import './BlogPostPage-style.css'
 import CustomMap from './PeruMap'
 
+// import Slider from "react-slick";
+// import 'slick-carousel/slick/slick.css';
+// import 'slick-carousel/slick/slick-theme.css';
+// import ScrollAnimation from 'react-animate-on-scroll';
+// import "animate.css/animate.min.css";
+
+// function SampleNextArrow(props) {
+//   const { style, onClick } = props;
+//   return (
+//     <div
+//       className='arrow'
+//       style={{ 
+//         ...style, 
+//         transform: 'translateY(-20em) translateX(41.5vw)',
+//       }}
+//       onClick={onClick}
+//     />
+//   );
+// }
+
+// function SamplePrevArrow(props) {
+//   const { style, onClick } = props;
+//   return (
+//     <div
+//       className='arrow'
+//       style={{ 
+//         ...style, 
+//         transform: 'scaleX(-1) translateY(17.5em) translateX(7em)'
+//       }}
+//       onClick={onClick}
+//     />
+//   );
+// }
+
+// class SimpleSlider extends Component {
+//   render() {
+//     const settings = {
+//       dots: false,
+//       infinite: true,
+//       speed: 700,
+//       slidesToShow: 1,
+//       slidesToScroll: 1,
+//       adaptiveHeight: false,
+//       lazyload: true,
+//       autoplay: true,
+//       autoplaySpeed: 3000,
+//       pauseOnHover: true,
+//       pauseOnFocus: true,
+//       nextArrow: <SampleNextArrow />,
+//       prevArrow: <SamplePrevArrow />
+
+//     };
+//     return (
+//       <div>
+//         <Slider {...settings}>
+//           {this.props.children}
+//         </Slider>
+//       </div>
+//     );
+//   }
+// }
+
 const BlogPostPage = () => {
   const { slug } = useParams();
   const [postHtml, setPostHtml] = useState('');
@@ -31,7 +93,15 @@ const BlogPostPage = () => {
     <div dangerouslySetInnerHTML={{ __html: postHtml }} 
     className="markdown-content"
     ></div>
+      {/* <div
+      className='sliderimg'
+      >
+        <SimpleSlider>
+          {items}
+        </SimpleSlider>
+      </div> */}
     </div>
+    
   );
 };
 
