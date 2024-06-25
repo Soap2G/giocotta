84aebf1d8cbe93fea22ab77adf4708ae930cc156
Giovanni Guerrieri <{ID}+{username}@users.noreply.github.com>
2024-06-25 15:12:42 +0200

    day 0

--- src/posts/posts.jsx
+++ src/posts/posts.jsx
@@ -1,3 +1,14 @@
-const posts = [];
+const posts = [
+  {
+    "slug": "0",
+    "title": "0",
+    "path": "/posts/0.md"
+  },
+  {
+    "slug": "1",
+    "title": "1",
+    "path": "/posts/1.md"
+  }
+];
 
 export default posts;
