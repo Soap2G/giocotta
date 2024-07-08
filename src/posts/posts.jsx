79e253a36d8c1baa2bbb42721b49618b4c5471bc
Giovanni Guerrieri <{ID}+{username}@users.noreply.github.com>
2024-07-08 04:45:13 +0200

    day 13

--- src/posts/posts.jsx
+++ src/posts/posts.jsx
@@ -8,6 +8,66 @@ const posts = [
     "slug": "1",
     "title": "1",
     "path": "/posts/1.md"
+  },
+  {
+    "slug": "2",
+    "title": "2",
+    "path": "/posts/2.md"
+  },
+  {
+    "slug": "3",
+    "title": "3",
+    "path": "/posts/3.md"
+  },
+  {
+    "slug": "4",
+    "title": "4",
+    "path": "/posts/4.md"
+  },
+  {
+    "slug": "5",
+    "title": "5",
+    "path": "/posts/5.md"
+  },
+  {
+    "slug": "6",
+    "title": "6",
+    "path": "/posts/6.md"
+  },
+  {
+    "slug": "7",
+    "title": "7",
+    "path": "/posts/7.md"
+  },
+  {
+    "slug": "8",
+    "title": "8",
+    "path": "/posts/8.md"
+  },
+  {
+    "slug": "9",
+    "title": "9",
+    "path": "/posts/9.md"
+  },
+  {
+    "slug": "10",
+    "title": "10",
+    "path": "/posts/10.md"
+  },
+  {
+    "slug": "11",
+    "title": "11",
+    "path": "/posts/11.md"
+  },
+  {
+    "slug": "12",
+    "title": "12",
+    "path": "/posts/12.md"
+  },
+  {
+    "slug": "13",
+    "title": "13",
+    "path": "/posts/13.md"
   }
 ];
 
